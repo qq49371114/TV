ebdde51ec92dcff2b8f821a121481ef65252158b
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-08-01 23:28:31 +0800

    Add files via upload
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/leanback/java/com/fongmi/android/tv/ui/dialog/ConfigDialog.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/dialog/ConfigDialog.java
@@ -21,6 +21,7 @@ import com.fongmi.android.tv.event.ServerEvent;
 import com.fongmi.android.tv.impl.ConfigCallback;
 import com.fongmi.android.tv.server.Server;
 import com.fongmi.android.tv.ui.custom.CustomTextListener;
+import com.fongmi.android.tv.utils.CustomUtil;
 import com.fongmi.android.tv.utils.QRCode;
 import com.fongmi.android.tv.utils.ResUtil;
 import com.fongmi.android.tv.utils.UrlUtil;
@@ -80,11 +81,15 @@ public class ConfigDialog implements DialogInterface.OnDismissListener {
     }
 
     private void initView() {
-        binding.text.setText(url = getUrl());
+        String address = Server.get().getAddress();
+//        binding.text.setText(url = getUrl());
+        if (TextUtils.isEmpty(getName())) {
+            binding.text.setText(url = getUrl());
+        }
         binding.text.setSelection(TextUtils.isEmpty(url) ? 0 : url.length());
         binding.positive.setText(edit ? R.string.dialog_edit : R.string.dialog_positive);
-        binding.code.setImageBitmap(QRCode.getBitmap(Server.get().getAddress(3), 200, 0));
-        binding.info.setText(ResUtil.getString(R.string.push_info, Server.get().getAddress()).replace("，", "\n"));
+        binding.code.setImageBitmap(QRCode.getBitmap(address, 200, 0));
+        binding.info.setText(ResUtil.getString(R.string.push_info, address).replace("，", "\n"));
         binding.storage.setVisibility(PermissionX.isGranted(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) ? View.GONE : View.VISIBLE);
     }
 
@@ -118,6 +123,19 @@ public class ConfigDialog implements DialogInterface.OnDismissListener {
         }
     }
 
+    private String getName() {
+        switch (type) {
+            case 0:
+                return VodConfig.get().getConfig().getName();
+            case 1:
+                return LiveConfig.get().getConfig().getName();
+            case 2:
+                return WallConfig.get().getConfig().getName();
+            default:
+                return "";
+        }
+    }
+
     private void onStorage(View view) {
         PermissionX.init(activity).permissions(Manifest.permission.WRITE_EXTERNAL_STORAGE).request((allGranted, grantedList, deniedList) -> binding.storage.setVisibility(allGranted ? View.GONE : View.VISIBLE));
     }
@@ -143,7 +161,10 @@ public class ConfigDialog implements DialogInterface.OnDismissListener {
         String name = binding.name.getText().toString().trim();
         String text = UrlUtil.fixUrl(binding.text.getText().toString().trim());
         if (edit) Config.find(url, type).url(text).update();
-        if (text.isEmpty()) Config.delete(url, type);
+        if (text.isEmpty()) {
+            url = CustomUtil.getSource();
+            Config.find(url, 1).name(CustomUtil.getTitle()).update();
+        }
         if (name.isEmpty()) callback.setConfig(Config.find(text, type));
         else callback.setConfig(Config.find(text, name, type));
         dialog.dismiss();
