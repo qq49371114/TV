061d33b46ce7b19b5e5b4b3ef9c5546d34db7c90
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-10-05 23:10:05 +0800

    更新 Setting.java

--- app/src/main/java/com/fongmi/android/tv/Setting.java
+++ app/src/main/java/com/fongmi/android/tv/Setting.java
@@ -26,14 +26,6 @@ public class Setting {
         Prefers.put("proxy", proxy);
     }
 
-    public static String getJxtoken() {
-        return Prefers.getString("jx_token");
-    }
-
-    public static void putJxtoken(String jxToken) {
-        Prefers.put("jx_token", jxToken);
-    }
-
     public static String getKeep() {
         return Prefers.getString("keep");
     }
@@ -179,7 +171,7 @@ public class Setting {
     }
 
     public static int getEpisode() {
-        return Prefers.getInt("episode", 1);
+        return Prefers.getInt("episode");
     }
 
     public static void putEpisode(int episode) {
@@ -491,7 +483,7 @@ public class Setting {
     }
 
     public static int getHomeUI() {
-        return Prefers.getInt("home_ui", 0);
+        return Prefers.getInt("home_ui", 1);
     }
 
     public static void putHomeButtons(String buttons) {
@@ -523,7 +515,7 @@ public class Setting {
     }
 
     public static int getConfigCache() {
-        return Math.min(Prefers.getInt("config_cache", 1), 2);
+        return Math.min(Prefers.getInt("config_cache", 0), 2);
     }
 
     public static void putLanguage(int key) {
@@ -551,7 +543,7 @@ public class Setting {
     }
 
     public static boolean isRemoveAd() {
-        return Prefers.getBoolean("remove_ad", true);
+        return Prefers.getBoolean("remove_ad", false);
     }
 
     public static void putRemoveAd(boolean remove) {
