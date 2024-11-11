7e454b7c4cc5dfa3179f986cd5e87484f8ffe083
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-11-11 13:12:08 +0800

    更新 Button.java

--- app/src/leanback/java/com/fongmi/android/tv/bean/Button.java
+++ app/src/leanback/java/com/fongmi/android/tv/bean/Button.java
@@ -57,7 +57,6 @@ public class Button {
 
     public static String getDefaultButtons() {
         return Setting.getHomeUI() == 0 ? "0,1,2,3,4,5,6" : "1,2,3,4,5,6";
-        return Setting.getHomeUI() == 0 ? "0,2,3,4,5,6" : "2,3,4,5,6";
 
     }
 
