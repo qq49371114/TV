d17ad761806ee77e385ac7fc625d4ec4c4dd2168
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-11-18 10:43:20 +0800

    Update CustomUtil.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/utils/CustomUtil.java
+++ app/src/main/java/com/fongmi/android/tv/utils/CustomUtil.java
@@ -1,5 +1,7 @@
 package com.fongmi.android.tv.utils;
 
+import static com.github.catvod.net.OkHttp.isUrlReachable;
+
 import android.os.Handler;
 import android.os.Looper;
 
@@ -11,6 +13,8 @@ import com.google.gson.JsonParser;
 
 
 public class CustomUtil {
+
+    public static final String CHECK_URL = "http://47.109.61.116:86/config/check.txt";
 
     public static void clearCache(){
         JsonArray keysToDelete = new JsonArray();
@@ -95,9 +99,15 @@ public class CustomUtil {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                String url = "http://47.109.61.116:86/unify.json";
-//                System.out.println("initCache: 请求接口: " + url);
-                String data = OkHttp.string(url);
+                String data;
+                if (isUrlReachable(CHECK_URL, 5000)) {
+                    System.out.println("APP - 服务正常");
+                    String url = "http://47.109.61.116:86/unify.json";
+                    data = OkHttp.string(url);
+                } else {
+                    System.out.println("APP - 服务离线中");
+                    data = "";
+                }
 
                 // 使用 Handler 将结果传回主线程
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
