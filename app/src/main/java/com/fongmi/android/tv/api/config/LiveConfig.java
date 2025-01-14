66c49f6a8abb012250bc33c1e5f4c06e9eedd69d
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-01-14 21:27:34 +0800

    Update LiveConfig.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
@@ -74,7 +74,7 @@ public class LiveConfig {
     }
 
     public static boolean hasUrl() {
-        return  true; getUrl().length() > 0;
+        return  true; //getUrl().length() > 0;
     }
 
     public static void load(Config config, Callback callback) {
