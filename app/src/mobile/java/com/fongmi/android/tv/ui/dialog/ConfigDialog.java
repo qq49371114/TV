07842a73d0dc9f7b075d0aafd606b3dab83d1b17
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-01-14 21:46:40 +0800

    Update ConfigDialog.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/mobile/java/com/fongmi/android/tv/ui/dialog/ConfigDialog.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/dialog/ConfigDialog.java
@@ -120,11 +120,16 @@ public class ConfigDialog {
         }
     }
 
-    private void onPositive(DialogInterface dialog, int which) {
-        String url = binding.url.getText().toString().trim();
+       private void onPositive(DialogInterface dialog, int which) {
+        String url = UrlUtil.fixUrl(binding.url.getText().toString().trim());
         String name = binding.name.getText().toString().trim();
         if (edit) Config.find(ori, type).url(url).name(name).update();
-        if (url.isEmpty()) Config.delete(ori, type);
+//        if (url.isEmpty()) Config.delete(ori, type);
+        if (url.isEmpty()) {
+//            url = "assets://js/main.json";
+            url = "http://47.109.61.116:86/yylxnz.zip";
+            Config.find(url, 1).name("🐯遥遥领先🐯").update();
+        }
         callback.setConfig(Config.find(url, type));
         dialog.dismiss();
     }
