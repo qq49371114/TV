3dcc09c99a14a21a1c1426475f56349868fb9395
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-10-02 08:49:21 +0800

    Create Github.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

---
+++ catvod/src/main/java/com/github/catvod/utils/Github.java
@@ -0,0 +1,18 @@
+package com.github.catvod.utils;
+
+public class Github {
+
+    public static final String URL = "http://47.109.61.116:86";
+
+    private static String getUrl(String path, String name) {
+        return URL + "/" + path + "/" + name;
+    }
+
+    public static String getJson(boolean dev, String name) {
+        return getUrl("apk/" + (dev ? "dev" : "release"), name + ".json");
+    }
+
+    public static String getApk(boolean dev, String name) {
+        return getUrl("apk/" + (dev ? "dev" : "release"), name + ".apk");
+    }
+}
