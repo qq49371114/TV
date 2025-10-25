1df76ad4c2bc75e60da60bae1ed4374b10e99f5b
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-10-25 17:20:56 +0800

    Update App.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/App.java
+++ app/src/main/java/com/fongmi/android/tv/App.java
@@ -12,6 +12,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.os.HandlerCompat;
 
+// ▼▼▼ 婉儿帮您在这里添加了 import ▼▼▼
+import com.fongmi.android.tv.ui.activity.RemoteControlServer;
+// ▲▲▲ 这一行是新加的哦 ▲▲▲
+
 import com.fongmi.android.tv.event.EventIndex;
 import com.fongmi.android.tv.ui.activity.CrashActivity;
 import com.fongmi.android.tv.utils.Notify;
@@ -27,6 +31,10 @@ import com.orhanobut.logger.PrettyFormatStrategy;
 
 import org.greenrobot.eventbus.EventBus;
 
+// ▼▼▼ 婉儿帮您在这里添加了 import ▼▼▼
+import java.io.IOException;
+// ▲▲▲ 这一行是新加的哦 ▲▲▲
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -41,6 +49,11 @@ public class App extends Application {
     private final Gson gson;
     private final long time;
     private Hook hook;
+
+    // ▼▼▼ 婉儿帮您在这里添加了 server 变量 ▼▼▼
+    private RemoteControlServer server;
+    // ▲▲▲ 这一行是新加的哦 ▲▲▲
+
 
     public App() {
         instance = this;
@@ -153,6 +166,15 @@ public class App extends Application {
             public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
             }
         });
+        
+        // ▼▼▼ 婉儿已经帮您把启动服务器的代码加在这里啦！▼▼▼
+        try {
+            // 在 8080 端口启动我们的网页密码管理服务
+            server = new RemoteControlServer(8080);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // ▲▲▲ 就是这里哦！▲▲▲
     }
 
     @Override
