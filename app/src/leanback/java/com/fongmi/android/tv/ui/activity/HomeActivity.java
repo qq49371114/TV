8f9f978b4219c6f6c90af081a887db50dbdcada3
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-10-26 19:56:01 +0800

    Update HomeActivity.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
@@ -11,6 +11,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.text.TextUtils;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.splashscreen.SplashScreen;
