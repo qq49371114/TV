02ba6fb0d64f6b29e42184b7e131512cce7cff47
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-12-21 14:01:57 +0800

    Update Startup.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/Startup.java
+++ app/src/main/java/com/fongmi/android/tv/Startup.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.startup.Initializer;
 
-import com.fongmi.android.tv.event.EventIndex;
+//import com.fongmi.android.tv.event.EventIndex;
 import com.fongmi.android.tv.ui.activity.CrashActivity;
 import com.github.catvod.bean.Doh;
 import com.github.catvod.net.OkHttp;
@@ -27,7 +27,7 @@ public class Startup implements Initializer<Void> {
     public Void create(@NonNull Context context) {
         CaocConfig.Builder.create().trackActivities(true).backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT).errorActivity(CrashActivity.class).apply();
         Logger.addLogAdapter(new AndroidLogAdapter(PrettyFormatStrategy.newBuilder().methodCount(0).showThreadInfo(false).tag("TV").build()));
-        EventBus.builder().addIndex(new EventIndex()).installDefaultEventBus();
+        EventBus.builder().installDefaultEventBus();
         OkHttp.dns().setDoh(Doh.objectFrom(Setting.getDoh()));
         return null;
     }
