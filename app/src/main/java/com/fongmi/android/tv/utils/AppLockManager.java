fc19b18e365bb5481eb3103bdc881f310d6c9b32
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-12-22 22:08:13 +0800

    Create  AppLockManager.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

---
+++ app/src/main/java/com/fongmi/android/tv/utils/AppLockManager.java
@@ -0,0 +1,10 @@
+package com.fongmi.android.tv.utils; // 婉儿猜你的包名是这个，它会自动生成的
+
+/**
+ * 负责管理解锁后的“临时通行证”，防止无限锁屏
+ * @author 婉儿
+ */
+public class AppLockManager {
+    // 它真的就只有这一行有用的代码！
+    public static boolean isTemporarilyUnlocked = false;
+}
