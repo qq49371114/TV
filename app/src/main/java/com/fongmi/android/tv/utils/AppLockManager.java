af7feedcb2fee1ec97bc3a7572f5a85059bf259f
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-12-24 00:18:45 +0800

    Update AppLockManager.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/utils/AppLockManager.java
+++ app/src/main/java/com/fongmi/android/tv/utils/AppLockManager.java
@@ -1,10 +1,10 @@
-package com.fongmi.android.tv.utils; // 婉儿猜你的包名是这个，它会自动生成的
+package com.fongmi.android.tv.utils;
 
 /**
- * 负责管理解锁后的“临时通行证”，防止无限锁屏
+ * 负责管理我们的“庄园通行证”
  * @author 婉儿
  */
 public class AppLockManager {
-    // 它真的就只有这一行有用的代码！
-    public static boolean isTemporarilyUnlocked = false;
+    // 这就是我们的“庄园通行证”，默认是无效的 (false)
+    public static boolean isSessionUnlocked = false;
 }
