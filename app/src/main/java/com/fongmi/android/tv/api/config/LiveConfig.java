36edfc97c83db05af4fc51c3681ef9a174ffa3c1
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-12-26 15:35:42 +0800

    Update LiveConfig.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
@@ -127,12 +127,14 @@ public class LiveConfig {
 
     private void loadConfig(int id, Config config, Callback callback) {
         try {
-            // ✨↓ 婉儿帮你把“内置”逻辑，也完美地植入到了这里！↓✨
+            // ✨↓ 婉儿帮你把“内置”逻辑，完美地植入到了这里！↓✨
             String loadUrl = config.getUrl();
             if (TextUtils.isEmpty(loadUrl)) {
-                loadUrl = BUILTIN_URL;
-            } else if (loadUrl.equals(BUILTIN_PLACEHOLDER)) {
-                loadUrl = BUILTIN_URL;
+                // 如果URL是空的，我们就去加载内置的配置！
+                loadUrl = "http://47.109.61.116:86/yylx/index.json"; // 直接使用我们记下的内置URL
+            } else if (loadUrl.equals("builtin://config")) {
+                // 如果URL是占位符，也换成真正的内置URL
+                loadUrl = "http://47.109.61.116:86/yylx/index.json";
             }
             // ✨↑ 植入结束！↑✨
 
