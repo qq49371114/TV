209fca7de94b0c4407c863f83c6890c9fbfaea68
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-12-28 23:40:21 +0800

    Update WordPresenter.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/leanback/java/com/fongmi/android/tv/ui/presenter/WordPresenter.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/presenter/WordPresenter.java
@@ -6,10 +6,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.leanback.widget.Presenter;
 
+import com.bumptech.glide.Glide; // 引入Glide
 import com.fongmi.android.tv.bean.Word;
 import com.fongmi.android.tv.databinding.AdapterWordBinding;
 import com.fongmi.android.tv.utils.ImgUtil;
-import com.bumptech.glide.Glide;
 
 public class WordPresenter extends Presenter {
 
@@ -33,6 +33,12 @@ public class WordPresenter extends Presenter {
     public void onBindViewHolder(@NonNull Presenter.ViewHolder viewHolder, Object object) {
         Word.Data item = (Word.Data) object;
         ViewHolder holder = (ViewHolder) viewHolder;
+
+        // --- 核心修改1：每次工作前，先“打扫战场”！---
+        // 在加载新图片之前，先把旧的图片清理掉，防止复用时显示旧图。
+        holder.binding.image.setImageDrawable(null);
+
+        // 然后再开始布置新的展品
         holder.binding.text.setText(item.getTitle());
         ImgUtil.load(item.getTitle(), item.getPic(), holder.binding.image);
         setOnClickListener(holder, view -> listener.onItemClick(item));
@@ -40,6 +46,8 @@ public class WordPresenter extends Presenter {
 
     @Override
     public void onUnbindViewHolder(@NonNull Presenter.ViewHolder viewHolder) {
+        // --- 核心修改2：下班前，把自己的工具都收好！---
+        // 当这个“展台”被回收时，告诉Glide停止加载图片并释放资源，防止内存泄漏和图片错位。
         ViewHolder holder = (ViewHolder) viewHolder;
         Glide.with(holder.binding.image.getContext()).clear(holder.binding.image);
     }
