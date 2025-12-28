aad4925746d052b6177c09073b5210201a096222
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-12-28 21:33:08 +0800

    Update SmartNavDialog.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/ui/dialog/SmartNavDialog.java
+++ app/src/main/java/com/fongmi/android/tv/ui/dialog/SmartNavDialog.java
@@ -1,23 +1,41 @@
 package com.fongmi.android.tv.ui.dialog;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.leanback.widget.ArrayObjectAdapter;
+import androidx.leanback.widget.ItemBridgeAdapter;
 
 import com.fongmi.android.tv.R;
 import com.fongmi.android.tv.databinding.DialogSmartNavBinding;
+import com.fongmi.android.tv.ui.activity.SearchActivity;
+import com.fongmi.android.tv.ui.custom.SpaceItemDecoration;
+import com.fongmi.android.tv.ui.presenter.WordPresenter; // 我们借用一下现成的Presenter
 
-public class SmartNavDialog extends DialogFragment {
+import java.util.ArrayList;
+
+public class SmartNavDialog extends DialogFragment implements WordPresenter.OnClickListener {
 
     private DialogSmartNavBinding binding;
+    private ArrayObjectAdapter mRelatedAdapter;
+    private ArrayObjectAdapter mHotAdapter;
 
-    public static SmartNavDialog newInstance() {
-        return new SmartNavDialog();
+    // --- 核心修改1：newInstance方法现在可以接收数据了 ---
+    public static SmartNavDialog newInstance(ArrayList<String> relatedWords, ArrayList<String> hotWords) {
+        Bundle args = new Bundle();
+        args.putStringArrayList("related", relatedWords);
+        args.putStringArrayList("hot", hotWords);
+        SmartNavDialog fragment = new SmartNavDialog();
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Nullable
@@ -30,14 +48,71 @@ public class SmartNavDialog extends DialogFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // 我们已经把呼吸灯的View从布局里删掉了，所以这里也要把启动动画的代码删掉！
-        // TODO: 在这里，我们会接收推荐数据，并设置RecyclerView的Adapter
+        // 我们不再在这里启动动画，因为布局文件里已经把它删掉了
+        // startBreathingAnimation(); 
+        
+        // --- 核心修改2：设置RecyclerView ---
+        setRecyclerViews();
+        setData();
     }
+
+    private void setRecyclerViews() {
+        // 设置相关推荐列表
+        binding.relatedRecycler.setHasFixedSize(true);
+        binding.relatedRecycler.addItemDecoration(new SpaceItemDecoration(1, 16));
+        binding.relatedRecycler.setAdapter(new ItemBridgeAdapter(mRelatedAdapter = new ArrayObjectAdapter(new WordPresenter(this))));
+
+        // 设置热门推荐列表
+        binding.hotRecycler.setHasFixedSize(true);
+        binding.hotRecycler.addItemDecoration(new SpaceItemDecoration(1, 16));
+        binding.hotRecycler.setAdapter(new ItemBridgeAdapter(mHotAdapter = new ArrayObjectAdapter(new WordPresenter(this))));
+    }
+
+    private void setData() {
+        if (getArguments() == null) return;
+
+        ArrayList<String> relatedWords = getArguments().getStringArrayList("related");
+        ArrayList<String> hotWords = getArguments().getStringArrayList("hot");
+
+        if (relatedWords != null && !relatedWords.isEmpty()) {
+            mRelatedAdapter.setItems(relatedWords, null);
+        } else {
+            // 如果没有相关推荐，可以隐藏这部分
+            binding.relatedRecycler.setVisibility(View.GONE);
+            // 也可以显示一个提示，比如 "暂无相关推荐"
+        }
+
+        if (hotWords != null && !hotWords.isEmpty()) {
+            mHotAdapter.setItems(hotWords, null);
+        } else {
+            binding.hotRecycler.setVisibility(View.GONE);
+        }
+    }
+    
+    // --- 核心修改3：实现点击事件，启动一键搜索！---
+    @Override
+    public void onItemClick(String text) {
+        // 当任何一个推荐词被点击时，就启动搜索！
+        SearchActivity.start(getActivity(), text);
+        // 然后关闭自己
+        dismiss();
+    }
+
+    // --- 核心修改4：实现按键自动消失的功能 ---
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // 这里我们先用一个简单的方式，按返回键关闭
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            dismiss();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 
     @Override
     public void onStart() {
         super.onStart();
-        // 设置Dialog的样式，比如大小、位置、无标题栏等
         if (getDialog() != null && getDialog().getWindow() != null) {
             getDialog().getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         }
