0e05f0d63df6ade1b4e4bc0f5b23dc139d5aec91
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-12-29 20:27:06 +0800

    Update VideoActivity.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -100,6 +100,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.regex.Matcher;
+import java.util.stream.Collectors; // <--- ✨ 把这行加在这里！
 
 public class VideoActivity extends BaseActivity implements CustomKeyDownVod.Listener, TrackDialog.Listener, ArrayPresenter.OnClickListener, Clock.Callback {
 
