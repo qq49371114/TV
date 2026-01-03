9a9234a18ab613d5c69381bd8b0978e372247f8c
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2026-01-03 08:00:05 +0800

    Update AdFilter.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/player/AdFilter.java
+++ app/src/main/java/com/fongmi/android/tv/player/AdFilter.java
@@ -11,7 +11,7 @@ public class AdFilter implements Interceptor {
     @NonNull @Override public Response intercept(@NonNull Chain chain) throws IOException {
         Request request = chain.request();
         String url = request.url().toString();
-        if (AdRule.get().isAd(url)) {
+        if (AdRule.get().isAd(null, url)) {
             System.out.println("凤凰哨兵“源头扼杀”广告：" + url);
             return new Response.Builder().request(request).protocol(okhttp3.Protocol.HTTP_2).code(200).message("Blocked").body(ResponseBody.create("", null)).build();
         }
