847065390d748ae5bcfe723edee6630d819ab1bb
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2026-01-03 12:37:21 +0800

    Update TimeSlot.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/model/TimeSlot.java
+++ app/src/main/java/com/fongmi/android/tv/model/TimeSlot.java
@@ -1,23 +1,62 @@
 package com.fongmi.android.tv.model;
 
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
 /**
- * 描述一个允许使用的时间段
+ * 描述一个允许使用的时间段 (已升级为支持星期规则)
  * @author 婉儿
  */
 public class TimeSlot {
+
+    @SerializedName("days")
+    private List<Integer> days;
+
+    @SerializedName("startHour")
     public int startHour;
+
+    @SerializedName("startMinute")
     public int startMinute;
+
+    @SerializedName("endHour")
     public int endHour;
+
+    @SerializedName("endMinute")
     public int endMinute;
 
     // 空的构造函数是给Gson用的
     public TimeSlot() {
     }
-
-    public TimeSlot(int startHour, int startMinute, int endHour, int endMinute) {
+    
+    public TimeSlot(List<Integer> days, int startHour, int startMinute, int endHour, int endMinute) {
+        this.days = days;
         this.startHour = startHour;
         this.startMinute = startMinute;
         this.endHour = endHour;
         this.endMinute = endMinute;
     }
+
+    // --- ✨↓ 婉儿把所有需要的方法都放在这里啦！↓✨ ---
+
+    public List<Integer> getDays() {
+        return days;
+    }
+
+    public int getStartHour() {
+        return this.startHour;
+    }
+
+    public int getStartMinute() {
+        return this.startMinute;
+    }
+
+    public int getEndHour() {
+        return this.endHour;
+    }
+
+    public int getEndMinute() {
+        return this.endMinute;
+    }
+    
+    // --- ✨↑ 方法区结束！↑✨ ---
 }
