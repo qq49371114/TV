5bcd18a149c92fda2532e0d676387ac3ab45d7d0
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2026-01-05 23:24:30 +0800

    Update AdSwitch.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/player/AdSwitch.java
+++ app/src/main/java/com/fongmi/android/tv/player/AdSwitch.java
@@ -23,8 +23,8 @@ import okhttp3.Response;
  */
 public class AdSwitch {
     private static final String PREFS_NAME = "ad_switch_prefs";
-    private static final String KEY_USER_INPUT_CODE = "user_input_code"; // ✨ 我们现在只保存验证通过的“明文码”
-    private static final String KEY_VALID_CODES_CACHE = "valid_codes_cache";
+    private static final String KEY_USER_INPUT_CODE = "ThisIsActKey123!"; // ✨ 我们现在只保存验证通过的“明文码”
+    private static final String KEY_VALID_CODES_CACHE = "ThisIsActIv1234!";
 
     private static final byte[] LIST_DECRYPT_KEY = "PHOENIX-LIST-KEY".getBytes();
     private static final byte[] LIST_DECRYPT_IV  = "PHOENIX-LIST-IV!".getBytes();
