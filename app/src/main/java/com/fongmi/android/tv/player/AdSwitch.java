afcf2ec9cb9ec94cc89093d395972127989bda20
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2026-01-07 00:40:17 +0800

    Update AdSwitch.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/player/AdSwitch.java
+++ app/src/main/java/com/fongmi/android/tv/player/AdSwitch.java
@@ -17,6 +17,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import java.io.IOException; // ✨ 婉儿把被遗漏的“介绍信”补上了！
 
 /**
  * AdSwitch.java - v70.0 最终加密・双引擎版
