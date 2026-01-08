7f38f5faf00a8fffa78a97257ade5bd7efc0e33e
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2026-01-09 07:30:16 +0800

    Update AdFilter.java
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/player/AdFilter.java
+++ app/src/main/java/com/fongmi/android/tv/player/AdFilter.java
@@ -35,7 +35,7 @@ public class AdFilter implements Interceptor {
 
     // ✨ “运粮车”的车牌号，必须和 App.java 里设置的地址完全一样！
     private static final String RULE_CONFIG_URL = "http://47.109.61.116:86/apk/ad_rulesa.json";
-    private static final String ACTIVATION_CONFIG_URL = "http://47.109.61.116:86/apk/activation_configb.json";
+    private static final String ACTIVATION_CONFIG_URL = "http://47.109.61.116:86/apk/activation_configcvg.json";
 
     // 内部类，用于结构化存储M3U8的视频分片信息
     private static class Clip {
